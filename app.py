9e36ab8018c7320c6ce614608868aba4739e9bf0
Ivan <user-email@go.uk>
2025-09-29 12:02:02 +0300

    add lesson1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask  
+app = Flask(__name__)    
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    return 'Hello, world!'
+
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+    return "This is your homepage :) "
+
+if __name__ == "__main__":
+    app.run()  # Launch built-in web server and run this Flask webapp, debug=True
