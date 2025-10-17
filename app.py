866e5e969d0eed5f996409e15c0696945e0ffe2a
Ivan <user-email@go.uk>
2025-10-17 14:31:40 +0300

    update cookie

--- app.py
+++ app.py
@@ -1,8 +1,59 @@
-from flask import Flask, request, redirect, url_for, render_template, abort
+from flask import Flask, request, redirect, url_for, render_template, abort, make_response, session
 from utils.repo import product_repo
+from datetime import timedelta, datetime
+
 
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+app.secret_key = "secret_key_12345"
+#app.config.from_pyfile("config.py")
+
+@app.route("/login",  methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form["login"]
+        session["username"] = username
+        return redirect(url_for("get_profile"))
+    return render_template("login.html")
+
+
+@app.route("/profile")
+def get_profile():
+    if "username" in session:
+        username_value = session["username"]
+        return render_template("profile.html", username=username_value)
+    return redirect(url_for("login"))
+
+
+
+@app.route('/logout')
+def logout():
+    # Видалення користувача із сесії
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', '', max_age=timedelta(seconds=60))
+    return response
+
+@app.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@app.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    #response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    response.delete_cookie('color')
+    return response
+
+
+
 
 
 @app.route('/products') 
